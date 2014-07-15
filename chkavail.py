7c890725efc2aa425ce7ec859afc455c0bdedf56
ambaker-usgs <ambaker@asl-dhcp108.cr.usgs.gov>
2014-07-15 15:19:20 -0600

    Added flexibility, indiscriminate IRIS checking
    -Added flexibility through the use of more functions, took out and
    removed some of the repetitive redundancy.
    - Added IRIS as a parser flag. Please note that flagging IRIS in the
    command forces IRIS to be queried. Without the flag set it will check
    IRIS only in the event that /xs0 or /xs1 is less than 100%.
    - Changed the verbose output. Removed traces.

--- chkavail.py
+++ chkavail.py
@@ -19,14 +19,14 @@ from multiprocessing import Pool
 #Methods
 #args()
 #checkAvail()
-#nptsSum()
 #
 ###################################################################################################
 
-print 'Scan started on', strftime("%Y-%m-%d %H:%M:%S", gmtime()), 'UTC'
+print 'Scan started on', strftime('%Y-%m-%d %H:%M:%S', gmtime()), 'UTC'
 
 client = Client("IRIS")
 chan = '*[BL]H*'
+decimalPlaces = 2
 
 
 
@@ -34,38 +34,47 @@ def args():
 #This function parses the command line arguments
 	parser = argparse.ArgumentParser(description='Code to compare data availability')
 
+#Sets flag for the network
 	parser.add_argument('-n', action = "store",dest="net", \
 		default = "*", help="Network to check: NN", type = str, required = True)
 
+#Sets flag for the station (optional)
 	parser.add_argument('-s', action = "store",dest="sta", \
 		default = "*", help="Station to check: SSSS", type = str, required = False)
 
+#Sets flag for the year in range
 	parser.add_argument('-y', action = "store",dest="year", \
 		default = 2014, help="Year to check: YYYY", type = int, required = True)
 
+#Sets flag for the first day in range
 	parser.add_argument('-sd', action = "store",dest="sday", \
 		default = 1, help="Start day: DDD", type = int, required = True)
 
+#Sets flag for the last day in range (optional)
 	parser.add_argument('-ed', action = "store",dest="eday", \
 		default = 1, help="End day: DDD", type = int)
 
-#Here is verbose mode
+#Sets flag for verbose mode
 	parser.add_argument('-d','--debug',action = "store_true",dest="debug", \
 		default = False, help="Run in debug mode")
 
-#Here is the NEIC CWB
+#Sets flag for forcefully checking IRIS
+	parser.add_argument('-IRIS',action = "store_true",dest="iris", \
+		default = False, help="Indiscriminately check IRIS without regard to /xsX")
+
+#Sets flag for the NEIC CWB
 	parser.add_argument('-GCWB',action = "store_true",dest="gcwb", \
 		default = False, help="Check the NEIC CWB (GCWB)")
 
-#Here is the PCWB
+#Sets flag for the PCWB
 	parser.add_argument('-PCWB',action = "store_true",dest="pcwb", \
 		default = False, help="Check the NEIC internal CWB (PCWB)")
 
-#Here is the ASL CWB
+#Sets flag for the ASL CWB
 	parser.add_argument('-ASL',action = "store_true",dest="asl", \
 		default = False, help="Check the ASL CWB (ASLCWB)")
 
-#Here is the quality flag for Tyler
+#Sets flag for IRIS data quality type for Tyler
 	parser.add_argument('-q',action = "store",dest = "quality", \
 		default = "M", help = "Data quality type: D,Q,...")
 
@@ -73,233 +82,166 @@ def args():
 	return parserval
 
 
-def checkAvail(string):
+def checkDate(date):
 #Here is the heart of the program where the availability gets checked
-
-#Setup a path to decide if it is on /xs1 or /xs0	
-	if net in set(["IU","IC","CU"]):
-		netpath = "xs0"
-	else:
-		netpath = "xs1"
-
 	allAvailString=[]
 
 #Here we parse the function arguments
-	string = string.split(",")
-	year = int(string[0])
-	jday = int(string[1])
+	year, day = date.split(",")
 	print 'On day ' + str(jday).zfill(3) + ' ' + str(year)
 
-	startTime = UTCDateTime(str(year) + str(jday).zfill(3) + "T00:00:00.000")
+	startTime = UTCDateTime(year + day.zfill(3) + 'T00:00:00.000')
 	endTime = startTime + 24*60*60
 
 #Setup the /tr1 string and glob it
-	globString = '/tr1/telemetry_days/' + net + '_' + sta +'/' + str(year) + '/' + \
-		str(year) + '_' + str(jday).zfill(3) + '/' + chan
-	dataOnTr1 = glob.glob(globString)
-
-	if len(dataOnTr1) == 0:
-		#If there is no data on /tr1/ then it checks /xs0/ or /xs1/ accordingly
-		if net in set(["IU","IC","CU"]):
-			globString = '/xs0/seed/' + net + '_' + sta +'/' + str(year) + '/' + \
-				str(year) + '_' + str(jday).zfill(3) + '_' + net + '_' + sta + '/' + chan
-			dataOnTr1 = glob.glob(globString)
-		else:
-			globString = '/xs1/seed/' + net + '_' + sta +'/' + str(year) + '/' + \
-				str(year) + '_' + str(jday).zfill(3) + '_' + net + '_' + sta + '/' + chan
-			dataOnTr1 = glob.glob(globString)
+	if net in ['IU','IC','CU']:
+		netPath = '/xs0'
+	else:
+		netPath = '/xs1'
+	globString = netPath + '/seed/' + net + '_' + sta +'/' + year + '/' + year + '_' \
+		+ day.zfill(3) + '_' + net + '_' + sta + '/' + chan
+	dataOnXSX = glob.glob(globString)
 	
 	if debug:
 		print(globString)
-		print(dataOnTr1)
+		for item in dataOnXSX:
+			print item
 
 
-	for index, dataTrace in enumerate(dataOnTr1):
+	for tracePath in dataOnXSX:
+		#Parses through the different channels and locations for a given station
+		#Below sets up the availability string as laid out in the header
+		currentAvailability = identifyChanLoc(tracePath)
 		#if True:
 		try:
 			if debug:
 				#To visually separate the channel location scans from one another
-				print '\n' + '*' * 80 + '\n' + '*' * 80
-#tr1 availability
-			if 'tr1' in dataTrace:
-				trtr1 = read(dataTrace)
-				if debug:
-					nptsBefore = nptsSum(trtr1)
-					print '\ntrTR1 points:', nptsBefore, '\n', trtr1
-				availtr1 = 0
-				for tr in trtr1:
-					availtr1 += tr.stats.npts / (24*60*60*tr.stats.sampling_rate)
-				if debug:
-					print 'tr1 avialability:', str(availtr1 * 100) + '%'
+				print '\n' + '*' * 80
+			#Checks the availability for given sources
+			availXSX        = checkAvailability('xsX', tracePath, startTime, endTime)
+			availTR1        = checkAvailability('tr1', tracePath, startTime, endTime)
+			if availXSX < 100.0 or parserval.iris:
+				availIRIS   = checkAvailability('IRIS', tracePath, startTime, endTime)
 			else:
-				availtr1 = 0
-		
-#Here is the xs0 availability
-			if 'tr1' in dataTrace:
-				#If data exists on /tr1/, replace its dataTrace to now look at /xs0/
-				trxs0 = dataTrace.replace('/tr1/telemetry_days','/' + netpath + '/seed')
-				trxs0 = trxs0.replace(str(year) + '_' + str(jday).zfill(3), \
-					str(year) + '_' + str(jday).zfill(3) + '_' + net + '_' + sta)
-				trxs0 = read(trxs0)
-			else:
-				trxs0 = read(dataTrace)
-			if debug:
-				nptsBefore = nptsSum(trxs0)
-				print '\ntrXS0 points:', nptsBefore, '\n', trxs0
-			availxs0 = 0
-			for tr in trxs0:
-				availxs0 += tr.stats.npts / (24*60*60*tr.stats.sampling_rate)
-			if debug:
-				print netpath + ' avail: ' + str(availxs0*100) + '%'
-
-#Here is the IRIS availability
-			if availxs0 < 0.999 or availtr1 < 0.999:
-				try:
-					trIRIS = client.get_waveforms(net,trxs0[0].stats.station, \
-						trxs0[0].stats.location, trxs0[0].stats.channel, \
-						startTime,endTime,quality=qualval)
-					if debug:
-						nptsBefore = nptsSum(trIRIS)
-						print '\ntrIRIS points:', nptsBefore, '\n', trIRIS
-					availIRIS = 0
-					for tr in trIRIS:
-						availIRIS += tr.stats.npts / (24*60*60*tr.stats.sampling_rate)
-				except:
-					availIRIS = 0
 				if debug:
-					print 'IRIS availability: ' + str(availIRIS*100) + '%'
-
-
-#Here we check the GCWB availability
-				availIRIS = round(availIRIS,3)
-				availxs0 = round(availxs0,3)
-				availtr1 = round(availtr1,3)
-				availString = trIRIS[0].stats.station + ',' + trIRIS[0].stats.location + \
-							',' + trIRIS[0].stats.channel + ',' + str(year) + ',' + \
-							str(jday).zfill(3) + ',' + str(availIRIS*100) + \
-							','  + str(availxs0*100) + ',' + str(availtr1*100)
-
-
-				if parserval.pcwb:
-					try:
-						trPCWB = clientPCWB.getWaveform(net,trxs0[0].stats.station, \
-							trxs0[0].stats.location, trxs0[0].stats.channel, \
-							startTime,endTime)
-						if debug:
-							nptsBefore = nptsSum(trPCWB)
-							print '\ntrPCWB points:', nptsBefore, '\n', trPCWB
-						availPCWB = 0
-						for tr in trPCWB:
-							availPCWB += tr.stats.npts / (24*60*60*tr.stats.sampling_rate)
-						availPCWB = round(availPCWB,3)
-					except:
-						print 'Unable to access PCWB'
-						availPCWB = 0
-					if debug:
-						print 'PCWB avialability:', availPCWB * 100
-					if availIRIS != availxs0 or availIRIS != availtr1 or availPCWB != availtr1:
-						availString += ',' + str(availPCWB*100)
-						allAvailString.append(availString)
-						print availString
-				if parserval.gcwb and not parserval.asl:
-					try:
-						trGCWB = clientGCWB.getWaveform(net,trxs0[0].stats.station, \
-							trxs0[0].stats.location, trxs0[0].stats.channel, \
-							startTime,endTime)
-						if debug:
-							nptsBefore = nptsSum(trGCWB)
-							print '\ntrGCWB points:', nptsBefore, '\n', trGCWB
-						availGCWB = 0
-						for tr in trGCWB:
-							availGCWB += tr.stats.npts / (24*60*60*tr.stats.sampling_rate)
-						availGCWB = round(availGCWB,3)
-					except:
-						availGCWB = 0
-					if debug:
-						print 'GCWB avialability:', availGCWB * 100
-					if availIRIS != availxs0 or availIRIS != availtr1 or availGCWB != availtr1:
-						availString += ',' + str(availGCWB*100) 
-						allAvailString.append(availString)
-						print availString
-				elif parserval.asl and not parserval.gcwb:
-					try:
-						trASL = clientASL.getWaveform(net,trxs0[0].stats.station, \
-							trxs0[0].stats.location, trxs0[0].stats.channel, \
-							startTime,endTime)
-						if debug:
-							nptsBefore = nptsSum(trASL)
-							print '\ntrASL points:', nptsBefore, '\n', trASL
-						availASL = 0
-						for tr in trASL:
-							availASL += tr.stats.npts / (24*60*60*tr.stats.sampling_rate)
-						availASL = round(availASL,3)
-					except:
-						availASL = 0
-					if debug:
-						print 'ASLCWB avialability:', availASL * 100
-					if availIRIS != availxs0 or availIRIS != availtr1 or availASL != availtr1:
-						availString += ',' + str(availASL*100) 
-						allAvailString.append(availString)
-						print availString
-
-				elif parserval.asl and parserval.gcwb:
-					trASL = clientASL.getWaveform(net,trxs0[0].stats.station, \
-						trxs0[0].stats.location, trxs0[0].stats.channel, \
-						startTime,endTime)
-					if debug:
-						nptsBefore = nptsSum(trASL)
-						print '\ntrASL points:', nptsBefore, '\n', trASL
-					availASL = 0
-					for tr in trASL:
-						availASL += tr.stats.npts / (24*60*60*tr.stats.sampling_rate)
-					if debug:
-						print 'ASLCWB avialability:', availASL * 100
-					availASL = round(availASL,3)
-					
-					trGCWB = clientGCWB.getWaveform(net,trxs0[0].stats.station, \
-						trxs0[0].stats.location, trxs0[0].stats.channel, \
-						startTime,endTime)
-					if debug:
-						nptsBefore = nptsSum(trGCWB)
-						print '\ntrGCWB points:', nptsBefore, '\n', trGCWB
-					availGCWB = 0
-					for tr in trGCWB:
-						availGCWB += tr.stats.npts / (24*60*60*tr.stats.sampling_rate)
-					if debug:
-						print 'GCWB avialability:', availGCWB * 100
-					availGCWB = round(availGCWB,3)
-					
-					if availIRIS != availxs0 or availIRIS != availtr1 or availGCWB != availtr1 or availASL != availtr1:
-						availString += ',' + str(availGCWB*100) + ',' + str(availASL*100)
-						allAvailString.append(availString)
-						print availString
-
-
-
-
-				else:
-					if availIRIS != availxs0 or availIRIS != availtr1:
-						allAvailString.append(availString)
-						print availString
-
+					print '\nIRIS not queried due to', netPath, 'availability percentage'
+				availIRIS   = 'XX'
+			if parserval.gcwb:
+				availGCWB   = checkAvailability('GCWB', tracePath, startTime, endTime)
+			if parserval.pcwb:
+				availPCWB   = checkAvailability('PCWB', tracePath, startTime, endTime)
+			if parserval.asl:
+				availASLCWB = checkAvailability('ASLCWB', tracePath, startTime, endTime)
+			#Appends the availability percentages for chan/loc for given station
+			currentAvailability.append(str(availXSX))
+			currentAvailability.append(str(availTR1))
+			currentAvailability.append(str(availIRIS))
+			if parserval.gcwb:
+				currentAvailability.append(str(availGCWB))
+			if parserval.pcwb:
+				currentAvailability.append(str(availPCWB))
+			if parserval.asl:
+				currentAvailability.append(str(availASLCWB))
+			if availXSX < 100.0:
+				#If /xs0 or /xs1 availability is not 100%
+				if availXSX != availIRIS:
+					#And if /xs0 or /xs1 availability does not equal IRIS
+					allAvailability.append(','.join(currentAvailability))
+			if debug:
+				#Prints the station/chan/loc availability
+				print '\n', header[:-1]
+				print ' '.join(currentAvailability)
 		except:
-			print 'Problem with: ' + dataTrace
-	f = open("avail" + str(year) + net + '.csv',"a")
-	for curavail in allAvailString:
+			print 'Problem with:', tracePath
+	f = open(outputFilename(),'a')
+	for curavail in allAvailability:
+		#Writes the availability to file
 		print 'Writing to file'
-		f.write(curavail + "\n")
+		f.write(curavail + '\n')
 	f.close()
-	
 	return
 
-def nptsSum(traces):
-	npts = 0
-	for index in range(len(traces)):
-		npts += traces[index].stats.npts
-	return npts
+def checkAvailability(IDstring, tracePath, startTime, endTime):
+	#Checks the availability for the desired source
+	try:
+		if   IDstring == 'xsX':
+			#Checks for local data on /xs0 or /xs1 accordingly
+			traces = read(tracePath)
+		elif IDstring == 'tr1':
+			#Checks for local data on /tr1
+			#Note: data on /tr1 is only for the most current 100 day period
+			traces = read(convertXSXtoTR1(tracePath))
+		else:
+			#Checks CWBs
+			traces = getWaveform(IDstring, read(tracePath), startTime, endTime)
+		availability = 0
+		#Merges with method 0 to appropriately handle data overlaps
+		#Mostly an issue with IRIS data
+		traces.merge(method = -1)
+		if debug:
+			nptsSamples   = 0
+			sampleRate    = 0
+		for trace in traces:
+			#Sometimes merges have multiple traces and as such
+			#it is necessary to step through them one by one
+			availability += trace.stats.npts / (24*60*60 * trace.stats.sampling_rate)
+			if debug:
+				nptsSamples  += trace.stats.npts
+				sampleRate    = trace.stats.sampling_rate
+		if debug:
+			print ''
+			print IDstring, 'points:', nptsSamples, 'out of', sampleRate * 24 * 60 * 60
+			print IDstring, 'availability:', str(availability * 100) + '%'
+	except:
+		availability = 0
+		if debug:
+			print '\nProblem with ' + IDstring + ', availability set to 0%'
+			if IDstring == 'tr1' and not os.path.isfile(convertXSXtoTR1(tracePath)):
+				#Checks to see if the data exists on /tr1
+				print 'Data does not exist on /tr1 for this day'
+	return round(availability, decimalPlaces + 2) * 100
 
+def getWaveform(IDstring, trace, startTime, endTime):
+	#Gets the traces from CWBs
+	if   IDstring == 'IRIS':
+		#Incorporated Research Institutions for Seismology
+		trace = client.get_waveforms(net, trace[0].stats.station, trace[0].stats.location,\
+			trace[0].stats.channel, startTime, endTime, quality = qualval)
+	elif IDstring == 'GCWB':
+		#NEIC's (Golden's) public-facing CWB
+		trace = clientGCWB.getWaveform(net, trace[0].stats.station,\
+			trace[0].stats.location, trace[0].stats.channel, startTime, endTime)
+	elif IDstring == 'PCWB':
+		#NEIC's (internal) private-facing CWB
+		trace = clientPCWB.getWaveform(net, trace[0].stats.station,\
+			trace[0].stats.location, trace[0].stats.channel, startTime, endTime)
+	elif IDstring == 'ASLCWB':
+		#Albuquerque Seismological Laboratory's CWB
+		trace = clientASL.getWaveform(net, trace[0].stats.station,\
+			trace[0].stats.location, trace[0].stats.channel, startTime, endTime)
+	return trace
 
+def convertXSXtoTR1(tracePath):
+	#Converts the file paths from /xs0 or /xs1 to /tr1
+	tracePath    = tracePath.split('/')
+	netSta       = tracePath[3]
+	year         = tracePath[4]
+	yearDay      = tracePath[5][:8]
+	fileName     = tracePath[-1]
+	tr1TracePath = '/tr1/telemetry_days/' + netSta + '/' + year + '/' + yearDay + \
+		'/' + fileName
+	return tr1TracePath
 
+def identifyChanLoc(tracePath):
+	#Returns the preamble for station/loc/chan availability
+	tracePath = tracePath.split('/')
+	sta       = tracePath[3].split('_')[-1]
+	year      = tracePath[4]
+	day       = tracePath[5][5:8]
+	fileName  = tracePath[-1]
+	chan      = tracePath[-1][3:6]
+	loc       = tracePath[-1][:2]
+	return [sta, loc, chan, year, day]
 
 #Here are the parser value arguments
 #We make the global to get them in the function
@@ -309,6 +251,7 @@ year = parserval.year
 sta = parserval.sta
 debug = parserval.debug
 sday = parserval.sday
+
 if parserval.eday != 1:
 	#If eday flag was parsed, set eday
 	eday = parserval.eday
@@ -317,42 +260,60 @@ else:
 	#This allows for one-day scans
 	eday = sday
 qualval = parserval.quality
+
+if parserval.iris:
+	if debug:
+		print 'IRIS selected, will be indiscriminately queried'
+		
 if parserval.gcwb:
-	print 'GCWB selected', parserval.gcwb
+	#Sets up GCWB client to read the waveforms
+	if debug:
+		print 'GCWB selected', parserval.gcwb
 	clientGCWB = ClientGCWB()
 
 if parserval.asl:
-	print 'ASLCWB selected', parserval.asl
+	#Copies GCWB's client and redirects it to ASL's CWB
+	if debug:
+		print 'ASLCWB selected', parserval.asl
 	clientASL = ClientGCWB(host='136.177.121.27')
 
 if parserval.pcwb:
-	print 'PCWB selected', parserval.pcwb
+	#Copies GCWB's client and redirects it to their internal CWB
+	if debug:
+		print 'PCWB selected', parserval.pcwb
 	clientPCWB = ClientGCWB(host='136.177.24.70')
 
+def outputFilename():
+	#Forms the name of the output file
+	#Avail + year + jday + net (+ sta) .csv
+	outputFilename = 'avail' + str(year) + str(sday).zfill(3)
+	if net != '*':
+		outputFilename += net
+		if sta != '*':
+			outputFilename += sta
+	outputFilename += '.csv'
+	return outputFilename
 
-#Lets write the header to the csv file
-if os.path.isfile("avail" + str(year) + net):
-	os.remove("avail" + str(year) + net)
-
-f = open("avail" + str(year) + net + '.csv',"w")
-header = "Sta,Loc,Chan,Year,Day,IRIS,xs,tr1"
+f = open(outputFilename(),'w')
+header = 'Sta,Loc,Chan,Year,Day,xsX,tr1,IRIS'
 if parserval.pcwb:
-	header += ",PCWB"
+	header += ',PCWB'
 if parserval.gcwb:
-	header += ",GCWB"
+	header += ',GCWB'
 if parserval.asl:
-	header += ",ASLCWB"
+	header += ',ASLCWB'
 header += '\n'
 f.write(header)
 f.close()
 
 #Here we loop over the days
-sendToavail=[]
+dateRange = []
 for day in xrange(sday, eday + 1, 1):
-	sendToavail.append(str(year) + "," + str(day).zfill(3))
+	dateRange.append(str(year) + ',' + str(day).zfill(3))
 
 #Hwere we run everything as a multi-process
 pool = Pool()
-pool.map(checkAvail,sendToavail)
+pool.map(checkDate,dateRange)
 
-print 'Scan ended on', strftime("%Y-%m-%d %H:%M:%S", gmtime()), 'UTC'
+print 'Scan ended on', strftime('%Y-%m-%d %H:%M:%S', gmtime()), 'UTC'
+print 'Output saved to', outputFilename()

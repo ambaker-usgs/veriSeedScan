a1611dac1d6835efa7bfcf2b22602730bf4777a4
aringler-usgs <aringler@usgs.gov>
2014-07-17 15:30:43 -0600

    Started the first version to compare spectra

---
+++ veriPower.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+from obspy import read
+from obspy.signal import PPSD
+
+
+def getstations(net):
+	#Function to get the station to check
+	stationList = ['ANMO']
+
+
+	return stationList
+
+
+
+def argparse():
+	#Parse command line
+	return parserval
+
+
+
+def querydatabase():
+	#Here we query the database
+
+	return psdvalue
+
+def computePPSD():
+
+	return ppsdvalue
+
+
+def writeresults():
+
+	return
+
+
+def runmulti():
+	#This function will call PPSD, write results, and querydatabase
+	
+	return
+
+
+
+
+
+
+if __name__ == '__main__':
+	#Main function
+
+	#Parse command line
+
+	net ='IU'
+
+	#Get station list
+	stationList = getstations(net)
+	
+	#We will runmulti to do the multi-threading
+
+
+
+
+
+
+
+
+
+
